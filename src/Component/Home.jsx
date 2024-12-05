3979d87e78bd3a37f1fd3b9b494e58f17b68b356
Binodkurmi <bkurmi504@rku.ac.in>
2024-12-05 21:51:37 +0530

    home page edit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaHtml5, FaCss3Alt, FaJs, FaBootstrap, FaReact } from 'react-icons/fa';
+import { SiNodedotjs, SiMongodb, SiExpress, SiGithub, SiNpm, SiPostman } from 'react-icons/si';
+
 import '../Style/Home.css';
 
 const Home = () => {
@@ -14,6 +16,13 @@ const Home = () => {
           <FaJs className="icon js-icon" title="JavaScript" />
           <FaBootstrap className="icon bootstrap-icon" title="Bootstrap" />
           <FaReact className="icon react-icon" title="React" />
+          <SiNodedotjs className="icon nodejs-icon" title="Node.js" />
+					<SiMongodb className="icon mongodb-icon" title="MongoDB" />
+					<SiExpress className="icon express-icon" title="Express" />
+					<SiGithub className="icon github-icon" title="GitHub" />
+
+          {/* <SiNpm className="icon npm-icon" title="NPM" /> */}
+          {/* <SiPostman className="icon postman-icon" title="Postman" /> */}
         </div>
       </div>
     </div>
