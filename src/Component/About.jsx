643ba0448db3c6cbdf263b2287c6e58ae7f89219
Binodkurmi <bkurmi504@rku.ac.in>
2024-12-08 00:49:59 +0530

    footer

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
+// import { Link } from 'react-router-dom'; // Import Link for navigation
 import { FaHtml5, FaCss3Alt, FaJs, FaBootstrap, FaReact } from 'react-icons/fa';
 import { SiNodedotjs, SiMongodb, SiExpress, SiGithub } from 'react-icons/si';
 import '../Style/About.css';
