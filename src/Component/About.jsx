dd93ba8d8d62c66891a5f10d98ebf567542318d8
Binodkurmi <bkurmi504@rku.ac.in>
2024-12-08 00:35:47 +0530

    image change

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 import { FaHtml5, FaCss3Alt, FaJs, FaBootstrap, FaReact } from 'react-icons/fa';
 import { SiNodedotjs, SiMongodb, SiExpress, SiGithub } from 'react-icons/si';
 import '../Style/About.css';
@@ -9,12 +10,19 @@ const About = () => {
       <div className="about-left">
         <h1>About Me</h1>
         <p>
-				I am a full-stack developer skilled in both front-end and back-end technologies. I specialize in using React, CSS, and JavaScript for building responsive user interfaces, while also working with Node.js, Express, and MongoDB to develop robust back-end solutions. My goal is to create seamless and dynamic web applications that provide an excellent user experience and performance.
+          I am a full-stack developer skilled in both front-end and back-end technologies. I specialize in using React, CSS, and JavaScript for building responsive user interfaces, while also working with Node.js, Express, and MongoDB to develop robust back-end solutions. My goal is to create seamless and dynamic web applications that provide an excellent user experience and performance.
         </p>
-        <button className="about-btn">Contact Me</button>
+				<div>
+      {/* <Link to="/contact" className='contactme'>Contact Me</Link> */}
+    </div>
       </div>
       <div className="about-right">
-        <img src="/Profile.jpg" alt="Profile" className="About-image" style={{ width: '200px', height: 'auto' }} />
+				<img 
+					srcSet="/Profile@2x.jpg 2x, /Profile.jpg 1x" 
+					src="/Profile.jpg" 
+					alt="Profile" 
+					className="About-image" 
+			  />
         <div className="home-icons">
           <FaJs className="icon js-icon" title="JavaScript" />
           <FaReact className="icon react-icon" title="React" />
