cddf53acf6ee66f142374308329643572efa3e87
Binodkurmi <bkurmi504@rku.ac.in>
2024-12-08 01:11:28 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './Component/Header.jsx';  
-import Footer from './Component/Footer.jsx';
-import About from './Component/About.jsx';
-import Home from './Component/Home.jsx';
-import Skills from './Component/Skills.jsx';
-import Education from './Component/Education.jsx';
+import Header from './Component/Header';  
+import Footer from './Component/Footer';
+import About from './Component/About';
+import Home from './Component/Home';
+import Skills from './Component/Skills';
+import Education from './Component/Education';
 import './Style/App.css';
 
 const App = () => {
