8fc377edab048886bb77a7a636cf9a20cc853705
Binodkurmi <bkurmi504@rku.ac.in>
2025-02-11 22:37:18 +0530

    Renamed header.jsx to Header.jsx

---
+++ src/Component/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import '../Style/Header.css';
+
+const header = () => {
+	const [menuOpen, setMenuOpen] = useState(false); 
+
+	// Function to toggle the menu
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	return (
+		<header className="header">
+			<div className="container">
+				<div className="logo">Binod</div>
+				<nav className={`nav ${menuOpen ? 'open' : ''}`}>
+					<a href="#home" className="nav-link">Home</a>
+					<a href="#about" className="nav-link">About Me</a>
+					<a href="#education" className="nav-link">Education</a>
+					<a href="#skills" className="nav-link">Skills</a>
+					<a href="#contact" className="nav-link">Contact Me</a>
+				</nav>
+				<div className="hamburger" onClick={toggleMenu}> {/* When clicked, it calls toggleMenu to open/close the menu */}
+					<div className="line"></div>
+					<div className="line"></div>
+					<div className="line"></div>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default header;
