82c8bc3841bbb6bf39e32d46f8c4499452cc7dbb
Binodkurmi <bkurmi504@rku.ac.in>
2025-05-26 17:10:20 +0530

    asdfh

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -9,29 +9,30 @@ const About = () => {
         <h1>About Me</h1>
         <p>
           I am a full-stack developer skilled in both front-end and back-end
-          technologies. I specialize in using React, CSS, and JavaScript for
+          technologies. I specialize in using React, CSS, Tailwind CSS, and JavaScript for
           building responsive user interfaces, while also working with Node.js,
           Express, and MongoDB to develop robust back-end solutions. My goal is
           to create seamless and dynamic web applications that provide an
           excellent user experience and performance.
         </p>
-        <div>
-          {/* <Link to="/contact" className='contactme'>Contact Me</Link> */}
-        </div>
       </div>
+
       <div className="about-right">
         <img
           src="/Profile.jpg"
           alt="Profile"
           className="About-image"
         />
-        <div className="home-icons">
-          <FaJs className="icon js-icon" title="JavaScript" />
-          <FaReact className="icon react-icon" title="React" />
-          <SiNodedotjs className="icon nodejs-icon" title="Node.js" />
-          <SiMongodb className="icon mongodb-icon" title="MongoDB" />
-          <SiExpress className="icon express-icon" title="Express" />
-          <SiGithub className="icon github-icon" title="GitHub" />
+
+        <div className="icon-carousel">
+          <div className="icon-track">
+            <div className="icon-wrapper"><FaJs className="icon js-icon" title="JavaScript" /></div>
+            <div className="icon-wrapper"><FaReact className="icon react-icon" title="React" /></div>
+            <div className="icon-wrapper"><SiNodedotjs className="icon nodejs-icon" title="Node.js" /></div>
+            <div className="icon-wrapper"><SiMongodb className="icon mongodb-icon" title="MongoDB" /></div>
+            <div className="icon-wrapper"><SiExpress className="icon express-icon" title="Express" /></div>
+            <div className="icon-wrapper"><SiGithub className="icon github-icon" title="GitHub" /></div>
+          </div>
         </div>
       </div>
     </div>
